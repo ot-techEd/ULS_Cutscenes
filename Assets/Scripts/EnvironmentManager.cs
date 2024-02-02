953c527a1bd2c20da9c8546dde72cf603b9efde7
OT <oluwatosin.ogunyebi@techeducators.co.uk>
2024-02-02 10:07:06 +0000

    ULS First Update

---
+++ Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnvironmentManager: MonoBehaviour
+{
+    public Material milkywaySkybox;
+
+    public void SwapSkyboxMilkyWay()
+    {
+        RenderSettings.skybox = milkywaySkybox;
+    }
+}
